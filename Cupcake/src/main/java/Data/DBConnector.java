ee47273d85a629ec079cfdac2de2579ca58d6021
razz7 <rashem2@hotmail.com>
2019-02-28 10:21:16 +0100

    Test

---
+++ Cupcake/src/main/java/Data/DBConnector.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class DBConnector {
+    private Connection connection = null;
+	
+    //Constants
+    private static final String IP	 = "localhost"; 
+    private static final String PORT     = "3306";
+    public static final String DATABASE = "testbeforecupcakes";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "password";	     	
+	
+     public DBConnector() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+        String url = "jdbc:mysql://" + IP + ":" + PORT + "/" + DATABASE;
+        
+        this.connection = (Connection) DriverManager.getConnection(url, USERNAME, PASSWORD);
+     }
+    /*
+     public DBConnector() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+        String url = "jdbc:mysql://" + IP + ":" + PORT + "/" + DATABASE;
+        Properties props = new Properties();
+        props.put("user", USERNAME);
+        props.put("password", PASSWORD);
+        props.put("allowMultiQueries", true);
+        props.put("useUnicode", true);
+        props.put("useJDBCCompliantTimezoneShift", true);
+        props.put("useLegacyDatetimeCode", false);
+        props.put("serverTimezone", "CET");
+        this.connection = DriverManager.getConnection(url, props);
+        
+    }
+*/
+    public Connection getConnection() {
+        return this.connection;
+    }
+}
