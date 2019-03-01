b0afca5e3aaa3e1061fee8f8afe40eed95bad314
DitlevR <ditlev1997@hotmail.com>
2019-03-01 14:36:11 +0100

    a

--- Cupcake/src/main/java/Data/ShoppingCartMapper.java
+++ Cupcake/src/main/java/Data/ShoppingCartMapper.java
@@ -5,10 +5,40 @@
  */
 package Data;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 /**
  *
  * @author Rumle
  */
 public class ShoppingCartMapper {
+    
+    public void createOrder(Invoice invoice) {
+        
+        try {
+            DBConnector conn = new DBConnector();
+            Connection connection = conn.getConnection();
+            
+            for(int i = 0; i < invoice) 
+
+            String query = "INSERT INTO cupcake.orders (username, password, balance) VALUES (?,?,?);";
+
+            PreparedStatement pstmt = connection.prepareStatement(query);
+//            Statement statement = connection.createStatement();
+
+            pstmt.setString(1, user.getUserName());
+            pstmt.setString(2, user.getPassword());
+            pstmt.setDouble(3, user.getBalance());
+//            statement.executeUpdate(query);
+            pstmt.execute();
+            connection.close();
+        } catch (Exception e) {
+
+            e.getLocalizedMessage();
+        
+    }
+    
+    public void 
     
 }
