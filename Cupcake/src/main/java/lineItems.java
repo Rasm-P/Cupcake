3169fedb6becbcf7b38aa15d7bae055e7a251526
Ludvig Bramsen <cph-lb270@cphbusiness.dk>
2019-03-01 11:48:07 +0100

    a

--- Cupcake/src/main/java/lineItems.java
+++ Cupcake/src/main/java/lineItems.java
@@ -12,10 +12,20 @@ public class lineItems {
     String topping;
     String bottom;
     int quantity;
+    int fullPrice;
     
     public lineItems(int quantity, String topping, String bottom){
         this.quantity = quantity;
         this.topping = topping;
         this.bottom = bottom;
+    }
+    
+    public int getPrice(){
+        
+    }
+    
+    @Override
+    public String toString() {
+        return "You ordered " + quantity + " cupcakes with topping" + topping + " and bottom " + bottom + " costing " + fullPrice;
     }
 }
