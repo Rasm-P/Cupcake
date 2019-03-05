ca81460fa259d0b4fc89077c50126ddea17664fe
DitlevR <ditlev1997@hotmail.com>
2019-03-05 10:43:03 +0100

    a

--- Cupcake/src/main/java/Data/DataMapper.java
+++ Cupcake/src/main/java/Data/DataMapper.java
@@ -5,6 +5,7 @@
  */
 package Data;
 
+import Shop.Invoice;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -130,6 +131,288 @@ public class DataMapper {
        System.out.println(getInfo_Username_Password(username, password));
        
     }
+   
+   
+   
+   
+   
+   
+   
+   
+   /**
+ *
+ * @author Rumle
+ */
+
+    
+    public static void createOrder(Invoice invoice) throws Exception {
+        int invoice_number = 0;
+        int lineitems_number = 0;
+        try {
+            DBConnector conn = new DBConnector();
+            Connection connection = conn.getConnection();
+            
+              
+                
+            
+
+            String query = "INSERT INTO cupcake.invoice (idUser) VALUES (?);";
+
+            PreparedStatement pstmt = connection.prepareStatement(query);
+//            Statement statement = connection.createStatement();
+
+            pstmt.setInt(1, invoice.getUser().getIdUser());
+            
+            
+//            statement.executeUpdate(query);
+            pstmt.execute();
+            connection.close();
+            
+            
+            
+        } catch (Exception e) {
+
+            e.getLocalizedMessage();
+        
+    }
+        try {
+            DBConnector conn = new DBConnector();
+            Connection connection = conn.getConnection();
+            
+              
+                
+            
+
+            String query = "select invoice.invoice_id from invoice where idUser = " + invoice.getUser().getIdUser() + ";";
+
+            try ( // create the java statement
+                Statement st = connection.createStatement()) {
+                // execute the query, and get a java resultset
+                ResultSet rs = st.executeQuery(query);
+
+                // iterate through the java resultset
+                while (rs.next()) {
+                    invoice_number = rs.getInt("invoice_id");
+                    
+
+                }
+                connection.close();
+            }
+            
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+    
+    
+    try {
+            DBConnector conn = new DBConnector();
+            Connection connection = conn.getConnection();
+            
+              
+                
+            
+
+            String query = "INSERT INTO cupcake.orders (invoice_id, orderdate) VALUES (?,?);";
+
+            PreparedStatement pstmt = connection.prepareStatement(query);
+//            Statement statement = connection.createStatement();
+        System.out.println(invoice_number);
+            pstmt.setInt(1, invoice_number);
+            pstmt.setDate(2, Date.valueOf(LocalDate.now()));
+            
+//            statement.executeUpdate(query);
+            pstmt.execute();
+            connection.close();
+            
+            
+            
+        } catch (Exception e) {
+
+            e.getLocalizedMessage();
+        
+    }
+    
+    try {
+            DBConnector conn = new DBConnector();
+            Connection connection = conn.getConnection();
+            
+              
+                
+            
+
+            String query = "select orders.lineitems_id from orders where invoice_id = " + invoice_number + ";";
+
+            try ( // create the java statement
+                Statement st = connection.createStatement()) {
+                // execute the query, and get a java resultset
+                ResultSet rs = st.executeQuery(query);
+
+                // iterate through the java resultset
+                while (rs.next()) {
+                    lineitems_number = rs.getInt("lineitems_id");
+                    
+
+                }
+                connection.close();
+            }
+            System.out.println(lineitems_number);
+            System.out.println(invoice_number);
+            
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+    
+    for(int i = 0; i < invoice.getCart().getCart().size();i++) {
+                       
+         try {
+            DBConnector conn = new DBConnector();
+            Connection connection = conn.getConnection();
+            
+              
+                
+            
+
+            String query = "INSERT INTO cupcake.lineitems (lineitems_id, bottomname, toppingname, quantity) VALUES (?,?,?,?);";
+
+            PreparedStatement pstmt = connection.prepareStatement(query);
+//            Statement statement = connection.createStatement();
+ 
+    ArrayList<lineItems> list = invoice.getCart().getCart();
+             System.out.println(lineitems_number);
+             System.out.println("here");
+            pstmt.setInt(1, lineitems_number);
+            pstmt.setString(2, list.get(i).getCup().getBottom().getName());
+            pstmt.setString(3, list.get(i).getCup().getTop().getName());
+            pstmt.setDouble(4, list.get(i).getQuantity());
+//            statement.executeUpdate(query);
+            pstmt.execute();
+            connection.close();
+            
+            
+            
+        } catch (Exception e) {
+
+            e.getLocalizedMessage();
+        
+    }
+        }
+    }
+    
+    
+    
+    
+    
+        
+    
+    
+//    public static void main(String[] args) throws Exception {
+//        Bottoms bottom = new Bottoms("Chokolatechip", 10);
+//        Toppings top = new Toppings("Vanilla", 12);
+//        Bottoms bottom1 = new Bottoms("Chokolatechip", 10);
+//        Toppings top1 = new Toppings("Vanilla", 12);
+//        Bottoms bottom2 = new Bottoms("Chokolatechip", 10);
+//        Toppings top2 = new Toppings("Vanilla", 12);
+//        
+//        cupcake cup = new cupcake(bottom, top, String.valueOf(bottom.getPrice() + top.getPrice()));
+//        cupcake cup1 = new cupcake(bottom1, top1, String.valueOf(bottom1.getPrice() + top1.getPrice()));
+//        cupcake cup2 = new cupcake(bottom2, top2, String.valueOf(bottom2.getPrice() + top2.getPrice()));
+//        
+//        lineItems it = new lineItems(2, cup);
+//        lineItems it1 = new lineItems(5, cup1);
+//        lineItems it2 = new lineItems(10, cup2);
+//        Shop.shoppingCart cart = new Shop.shoppingCart();
+//        cart.getCart().add(it);
+//        cart.getCart().add(it1);
+//        cart.getCart().add(it2);
+//        
+//        User user = new User(2, "Hans", "qwe", Double.NaN);
+//        Invoice invoice = new Invoice(cart, user, LocalDate.now());
+//        //createOrder(invoice);
+//        saveShoppingCart(invoice);
+//    }
+    
+    
+    
+    public static void saveShoppingCart(Invoice invoice) {
+        
+        
+        
+        
+        
+        for(int i = 0; i < invoice.getCart().getCart().size();i++) {
+                       
+         try {
+            DBConnector conn = new DBConnector();
+            Connection connection = conn.getConnection();
+            
+              
+                
+            
+
+            String query = "INSERT INTO cupcake.lineitems (lineitems_id, bottomname, toppingname, quantity) VALUES (?,?,?,?);";
+
+            PreparedStatement pstmt = connection.prepareStatement(query);
+//            Statement statement = connection.createStatement();
+ 
+    ArrayList<lineItems> list = invoice.getCart().getCart();
+
+             System.out.println("here");
+            pstmt.setInt(1, 22);
+            pstmt.setString(2, list.get(i).getCup().getBottom().getName());
+            pstmt.setString(3, list.get(i).getCup().getTop().getName());
+            pstmt.setInt(4, list.get(i).getQuantity());
+//            statement.executeUpdate(query);
+            pstmt.execute();
+            connection.close();
+            
+            
+            
+        } catch (Exception e) {
+
+            e.getLocalizedMessage();
+        
+    }
+        }
+    }
+//    public static void main(String[] args) {
+//        Bottoms bottom = new Bottoms("Chokolatechip", 10);
+//        Toppings top = new Toppings("Vanilla", 12);
+//        Bottoms bottom1 = new Bottoms("Chokolatechip", 10);
+//        Toppings top1 = new Toppings("Vanilla", 12);
+//        Bottoms bottom2 = new Bottoms("Chokolatechip", 10);
+//        Toppings top2 = new Toppings("Vanilla", 12);
+//        
+//        cupcake cup = new cupcake(bottom, top, String.valueOf(bottom.getPrice() + top.getPrice()));
+//        cupcake cup1 = new cupcake(bottom1, top1, String.valueOf(bottom1.getPrice() + top1.getPrice()));
+//        cupcake cup2 = new cupcake(bottom2, top2, String.valueOf(bottom2.getPrice() + top2.getPrice()));
+//        
+//        lineItems it = new lineItems(2, cup);
+//        lineItems it1 = new lineItems(5, cup1);
+//        lineItems it2 = new lineItems(10, cup2);
+//        Shop.shoppingCart cart = new Shop.shoppingCart();
+//        //Invoice invoice = new Invoice(cart, )
+//        cart.addToCart(cup, 1);
+//        cart.addToCart(cup1, 1);
+//        cart.addToCart(cup2, 1);
+//        
+//        for(int i = 0; i < cart.getCart().size(); i++) {
+//            System.out.println(cart.getCart().get(i).toString());
+//        }
+//        saveShoppingCart(in);
+//        Shop.shoppingCart.getCart().add(cup);
+//        Shop.shoppingCart.getCart().add(cup1);
+//        Shop.shoppingCart.getCart().add(cup2);
+        
+        
+        
+        
+        
+        
+    }
+
+   
     
     
     
