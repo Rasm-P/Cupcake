faa0e5166387bd093029a59d6b3a69ed7235b044
DitlevR <ditlev1997@hotmail.com>
2019-03-05 13:20:32 +0100

    a

--- Cupcake/src/main/java/Users/User.java
+++ Cupcake/src/main/java/Users/User.java
@@ -5,6 +5,13 @@
  */
 package Users;
 
+import Data.DBConnector;
+import com.mysql.cj.jdbc.ha.BalanceStrategy;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  *
  * @author rh
@@ -45,6 +52,8 @@ private double balance;
     public double getBalance() {
         return balance;
     }
+    
+    
 
     public void setBalance(Double balance) {
         this.balance = balance;
@@ -62,7 +71,38 @@ private double balance;
         return idUser;
     }
 
+    public static double getBalanceFromDB(User user) {
+        double balance = 0;
+         String query = "SELECT user.balance from cupcake.user where username = " + user.getUserName() + " and password = " + user.getPassword()+ ";";
+         System.out.println(query);
+         try {
+            DBConnector conn = new DBConnector();
+            Connection connection = conn.getConnection();
+             Statement st = connection.createStatement(); {
+               
+                ResultSet rs = st.executeQuery(query); 
+            
+            
+            while (rs.next()) {
+                
+                    balance = rs.getDouble("balance");
+                System.out.println(balance);
+                }
+            
+            connection.close();
+         }
+            } catch (Exception e) {
+            e.getLocalizedMessage();
+        
+    }
+         return balance;
+        }
     
+    public static void main(String[] args) {
+        User user = new User(1, "Ole", "1234", 112.0);
+        getBalanceFromDB(user);
+        
+    }
     
 
     
