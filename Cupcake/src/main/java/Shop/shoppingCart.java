16a5361e443f2d6a2ed2ec6d32cfa7bc5740f0bb
razz7 <rashem2@hotmail.com>
2019-03-05 09:45:35 +0100

    a

--- Cupcake/src/main/java/Shop/shoppingCart.java
+++ Cupcake/src/main/java/Shop/shoppingCart.java
@@ -1,8 +1,8 @@
-<<<<<<< HEAD:Cupcake/src/main/java/shoppingCart.java
 
 import Cupcake.Bottoms;
 import Cupcake.Toppings;
 import Cupcake.cupcake;
+import Shop.lineItems;
 import java.util.ArrayList;
 
 /*
@@ -39,10 +39,10 @@ public class shoppingCart {
     public static void main(String [] args){
         Bottoms b1 = new Bottoms("Choco", 2.0);
         Toppings t1 = new Toppings("Banana", 3.0);
-        cupcake c1 = new cupcake(b1, t1, "5");
+        cupcake c1 = new cupcake(b1, t1, 5.0);
         
         Bottoms b2 = new Bottoms("Berry", 4.0);
-        cupcake c2 = new cupcake(b2, t1, "7");
+        cupcake c2 = new cupcake(b2, t1, 7.0);
         
         addToCart(c1, 4);
         addToCart(c1, 4);
@@ -60,44 +60,9 @@ public class shoppingCart {
 //        System.out.println(cart.get(2).getQuantity());
     }
 }
-=======
-package Shop;
 
 
-import Cupcake.cupcake;
-import java.util.ArrayList;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Ludvig
- */
-public class shoppingCart {
-    static ArrayList<lineItems> cart = new ArrayList(); 
-  
-    public void addToCart(cupcake cup, int qua){
-        for(int i = 0; i < cart.size(); i++){
-            if(cart.get(i).getCup().getBottom().equals(cup.getBottom()) && cart.get(i).getCup().getTop().equals(cup.getTop())){
-                cart.get(i).editQuantity(qua);
-            }
-        }
-        
-        cart.add(new lineItems(qua, cup));
-    }
-    
-    public static ArrayList getCart(){
-        return cart;               
-    }   
-    
-//    public static void main(String [] args){
-//        Bottoms b = new Bottoms("Choco", 2.0);
-//        Toppings t = new Toppings("Banana", 3.0);
-//        cupcake c = new cupcake(b, t, "2");
-//    }
-}
->>>>>>> 31318ebb4c7f67501461dde98bfcf63421e3db47:Cupcake/src/main/java/Shop/shoppingCart.java
+
+
