f15d5cc9eafd4a0d8277efa0d159a7495f3f842e
Rasm-P <44894132+Rasm-P@users.noreply.github.com>
2019-03-05 10:08:20 +0100

    a

--- Cupcake/src/main/java/Cupcake/Bottoms.java
+++ Cupcake/src/main/java/Cupcake/Bottoms.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -42,48 +41,3 @@ public class Bottoms {
     }
     
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Cupcake;
-
-/**
- *
- * @author rh
- */
-public class Bottoms {
-
-    private String name;
-    private double price;
-
-    public Bottoms(String name, double price) {
-        this.name = name;
-        this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }    
-
-    @Override
-    public String toString() {
-        return name + ", price " + price;
-    }
-    
-}
->>>>>>> b364da3fcf4ed35813666b53b94b338289d7b89d
--- Cupcake/src/main/java/OldCode/PageMakeLogin.java
+++ Cupcake/src/main/java/OldCode/PageMakeLogin.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Cupcake/src/main/java/OldCode/PageMakeLogin.java
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -46,52 +45,3 @@ public class PageMakeLogin {
     }
     
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Presentation;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.http.HttpServletResponse;
-
-/**
- *
- * @author Rasmus2
- */
-public class PageMakeLogin {
-
-    public static void generateMakeLogin(HttpServletResponse response) throws IOException {
-        System.out.println("makeing login");
-        try (PrintWriter out = response.getWriter())
-        {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet FrontController</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Create account!</h1>");
-            out.println("<p>");
-            out.println("<form>" +
-            "<input type=\"hidden\" name=\"action\" value=\"makeLogin\"><br>" +
-            "Username:<br>" +
-            "<input type=\"text\" name=\"username\" value=\"\"><br>" +
-            "Login:<br>" +
-            "<input type=\"text\" name=\"password\" value=\"\"><br><br>" +
-            "<input type=\"submit\" value=\"Create account\">" +
-            "</form>");
-            out.println("</p>");
-            out.println("<p><a href=\"/Cupcake/FrontController?action=login\">Log in</a></p>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-        System.out.println("made login");
-    }
-    
-}
->>>>>>> b364da3fcf4ed35813666b53b94b338289d7b89d:Cupcake/src/main/java/Presentation/PageMakeLogin.java
