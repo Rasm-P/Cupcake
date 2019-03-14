146a0111626469cd93497cca339338be688ed477
razz7 <rashem2@hotmail.com>
2019-03-14 13:57:53 +0100

    a

--- Cupcake/src/main/java/Data/DBConnector.java
+++ Cupcake/src/main/java/Data/DBConnector.java
@@ -8,7 +8,6 @@ package Data;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
